d3a038c9157d275c79f422704458bfc21f4abb33
SajibKC <sajibkumarchowdhury1883@gmail.com>
2025-07-23 12:26:54 +0600

    second commit

--- main.go
+++ main.go
@@ -1,19 +1,7 @@
 package main
 
-import (
-	apiHandler "example/Book_api_using_Golang/apiHandler"
-	"github.com/gin-gonic/gin"
-)
+import "example/Book_api_using_Golang/cmd"
 
 func main() {
-	router := gin.Default()
-
-	// Define routes for book operations.
-	router.GET("/books", apiHandler.GetBooks)
-	router.GET("/books/:id", apiHandler.GetBookByID)
-	router.POST("/books", apiHandler.CreateBook)
-	router.PUT("/books/:id", apiHandler.UpdateBookByID)
-	router.DELETE("/books/:id", apiHandler.DeleteBookByID)
-
-	router.Run("localhost:8080")
+	cmd.Execute()
 }
